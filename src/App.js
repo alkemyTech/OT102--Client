69df4ab10389554d6264dd0a35a3911c04c0df0b
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-11-30 17:07:21 -0300

    add Spinner component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from './logo.svg'
 import Counter from './features/counter/Counter'
 import './App.css'
+import Spinner from './components/Spinner'
 
 function App() {
   return (
@@ -9,11 +10,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+        <Spinner size="50" speed="1.5" />
         <p>
           Edit
-          {' '}
           <code>src/App.js</code>
-          {' '}
           and save to reload.
         </p>
         <span>
@@ -45,7 +45,7 @@ function App() {
             Redux Toolkit
           </a>
           ,
-          <span> and </span>
+          <span>and</span>
           <a
             className="App-link"
             href="https://react-redux.js.org/"
