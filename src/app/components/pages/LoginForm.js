510f66f973f50714d3d1f8ed12315a505939f085
gabodmc <gabrielmc@live.com.ar>
2021-12-02 11:39:08 -0300

    Chakra template added

--- src/app/components/pages/LoginForm.js
+++ src/app/components/pages/LoginForm.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Button,
+  Heading,
+} from '@chakra-ui/react'
 
 const LoginForm = () => {
   const formValues = []
+  console.log(formValues)
   const loginSchema = Yup.object().shape({
     email: Yup.string().email('Must be a valid email').max(255, 'Too long').required('Required'),
-    password: Yup.string().min(6, 'Password must have at least 6 caracters').max(255, 'Too long').required('Required'),
+    password: Yup.string()
+      .min(6, 'Password must have at least 6 caracters')
+      .max(255, 'Too long')
+      .required('Required'),
   })
 
   return (
@@ -26,36 +41,67 @@ const LoginForm = () => {
         }}
       >
         {({
-          values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting,
+          values, errors, touched, handleChange, handleBlur, handleSubmit,
         }) => (
           <form onSubmit={handleSubmit}>
-            <h3>Login</h3>
-            <label htmlFor="email">
-              Email
-              <input
-                type="email"
-                name="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-              />
-            </label>
-            <p>{errors.email && touched.email && errors.email}</p>
+            <Flex
+              minH="100vh"
+              align="center"
+              justify="center"
+            >
+              <Stack spacing={8} mx="auto" maxW="lg" py={12} px={6}>
+                <Stack align="center">
+                  <Heading fontSize="4xl">Sign in to your account</Heading>
+                </Stack>
+                <Box rounded="lg" boxShadow="lg" p={8}>
+                  <Stack spacing={4}>
+                    <FormControl id="email">
+                      <FormLabel>Email address</FormLabel>
+                      <Input
+                        type="email"
+                        name="email"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                      />
+                      <p>{errors.email && touched.email && errors.email}</p>
+                    </FormControl>
 
-            <label htmlFor="password">
-              Password
-              <input
-                type="password"
-                name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-              />
-            </label>
-            <p>{errors.password && touched.password && errors.password}</p>
-            <button type="submit" disabled={isSubmitting}>
-              {isSubmitting ? 'Enviando' : 'Enviar'}
-            </button>
+                    <FormControl id="password">
+                      <FormLabel>Password</FormLabel>
+                      <Input
+                        type="password"
+                        name="password"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                      />
+                      <p>{errors.password && touched.password && errors.password}</p>
+                    </FormControl>
+                    <Stack spacing={10}>
+                      <Stack
+                        direction={{ base: 'column', sm: 'row' }}
+                        align="start"
+                        justify="space-between"
+                      >
+                        <Checkbox>Remember me</Checkbox>
+                        {/* <Link color="blue.400">Forgot password?</Link> */}
+                      </Stack>
+                      <Button
+                        type="submit"
+                        bg="blue.400"
+                        color="white"
+                        _hover={{
+                          bg: 'blue.500',
+                        }}
+                      >
+                        Sign in
+                      </Button>
+                    </Stack>
+                  </Stack>
+                </Box>
+              </Stack>
+            </Flex>
           </form>
         )}
       </Formik>
