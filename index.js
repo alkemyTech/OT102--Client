b981646332e17ca48efd32c29ffc85b9a2352274
gabyMangoni <mangonigabriela@gmail.com>
2021-12-04 04:40:41 -0300

    correct error

---
+++ index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import theme from './themes'
+// eslint-disable-next-line
+import '@fontsource/atma/400.css'
+
+ReactDOM.render(
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>,
+  document.getElementById('root'),
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register()
