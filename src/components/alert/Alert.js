c57d8d4804825a753c28a4e2ec7caf3ec96fc5d2
Jose Ignacio Lorenzo <74208929+joselo85@users.noreply.github.com>
2021-12-04 18:11:35 -0300

    Initial Commit (#3)

---
+++ src/components/alert/Alert.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react'
+import swal from 'sweetalert2'
+import PropTypes from 'prop-types'
+
+import './Alert.css'
+
+/* USAGE: Alert component should receive as a props: show: boolean,
+title: string text: string,
+icon: 'error', 'success', 'warrning', 'info', 'question' or empty string ''.
+Cancel button (cancelbtn) boolean.
+if needed you can send as prop true and a cancel button will be displayed.
+onConfirm and onCancel are functions to be executed if user clicks on Accept or Cancel buttons */
+const Alert = ({
+  show, title, message, icon, cancelbtn, onConfirm, onCancel,
+}) => {
+  useEffect(async () => {
+    if (show) {
+      const result = await swal.fire({
+        position: 'center',
+        padding: '1rem 2rem',
+        confirmButtonColor: '#8DCAFF',
+        cancelButtonColor: '#EC4C4C',
+        title: `${title}`,
+        text: `${message}`,
+        icon: `${icon}`,
+        showCancelButton: cancelbtn,
+        allowOutsideClick: !cancelbtn, // If cancel button is displayed, do not allow outside click.
+      })
+      if (result.isConfirmed) {
+        onConfirm()
+      }
+      if (result.isDismissed) {
+        onCancel()
+      }
+    }
+  }, [show])
+  return <></>
+}
+
+Alert.defaultProps = {
+  icon: '',
+  cancelbtn: false,
+  onCancel: () => {},
+}
+
+Alert.propTypes = {
+  show: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+  cancelbtn: PropTypes.bool,
+  onConfirm: PropTypes.func.isRequired,
+  onCancel: PropTypes.func,
+}
+export default Alert
