51d368c836db07f21b6cde5e26ca012dc06a75b0
gabyMangoni <79479608+gabyMangoni@users.noreply.github.com>
2021-12-06 22:56:41 -0300

    [LAYOUT] Header - OT102-21 (#8)
    * Add Header Responsive
    * edit Header add navPublic
    * merge develop
    * merge develop package

---
+++ src/components/navPublic.js
@@ -0,0 +1,22 @@
+export const NAV_ITEMS = [
+  {
+    label: 'Nosotros',
+    href: '#',
+  },
+  {
+    label: 'Actividades',
+    href: '#',
+  },
+  {
+    label: 'Novedades',
+    href: '/news',
+  },
+  {
+    label: 'Testimonios',
+    href: '#',
+  },
+  {
+    label: 'Contactos',
+    href: '#',
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
-import store from './app/store'
 import * as serviceWorker from './serviceWorker'
+import theme from './themes'
+// eslint-disable-next-line
+import '@fontsource/atma/400.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </React.StrictMode>
+  </ChakraProvider>,
   document.getElementById('root'),
 )
 
