eff1bcb9e05967425ea89a2110c65d9c0301f812
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-12-07 23:11:01 -0300

    Link existing screens with the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Header from './components/Header'
+import Header from './components/header/Header'
 import Home from './components/Home'
 import News from './components/News'
 
