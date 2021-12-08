bddb692850dc993e56b85694ac32e017809707da
alexiscjscab <alexiscjscab@gmail.com>
2021-12-08 16:46:30 -0300

    Create contactSlice Reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import counterReducer from '../features/counter/counterSlice'
+import contactsreducer from '../features/contacts/contactsSlice'
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    contacts: contactsreducer,
   },
 })
