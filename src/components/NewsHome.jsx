16506f4008649e21f81eab81b4b4ee6738469d8f
gabyMangoni <mangonigabriela@gmail.com>
2021-12-08 20:52:33 -0300

    home, newsListHome, welcomeText

---
+++ src/components/NewsHome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Text } from '@chakra-ui/react'
+import NewsHomeList from './NewsHomeList'
+
+const NewsHome = () =>
+  (
+    <Container maxW="container.lg" mt="10">
+      <Text textStyle="title">Novedades</Text>
+      <NewsHomeList />
+    </Container>
+
+  )
+
+export default NewsHome
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
+import store from './app/store'
 import * as serviceWorker from './serviceWorker'
 import theme from './themes'
-// eslint-disable-next-line
-import '@fontsource/atma/400.css'
 
 ReactDOM.render(
   <ChakraProvider theme={theme}>
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   </ChakraProvider>,
   document.getElementById('root'),
