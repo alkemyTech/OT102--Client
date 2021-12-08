b91cb3c63ef9060d9a4ab83e2dabe84c0c24c45e
gabodmc <gabrielmc@live.com.ar>
2021-12-08 12:27:20 -0300

    Login user state with redux added. Minor correction in authservice

---
+++ src/app/slices/auth.js
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { userLogin } from '../../services/authService'
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    userData: {},
+  },
+  reducers: {
+    setUserData: (state, action) => {
+      state.userData = action.payload
+    },
+    destroyUserData: (state) => {
+      state.userData = {}
+    },
+  },
+})
+
+export const { setUserData, destroyUserData } = authSlice.actions
+
+/**
+ * Accepts an object to set the new state of user
+ * @async
+ * @param {object} token
+ * @param {string} token.token
+ * @param {object} user
+ * @param {string} user.firstName
+ * @param {string} user.lastName
+ * @param {string} user.email
+ * @param {string} user.password
+ * @return Promise {object} the logged user and set the new state with only body data.
+ */
+export const setUserDataAsync = (userData) => async (dispatch) => {
+  const response = await userLogin(userData)
+  dispatch(setUserData(response.data.body))
+}
+
+/**
+ * Function to get the user logged data in the state
+ * @param {string} token
+ * @param {object} user
+ * @param {string} user.firstName
+ * @param {string} user.lastName
+ * @param {string} user.email
+ * @param {string} user.password
+ * @return An {object} of the logged user state
+ */
+export const selectUserData = (state) => state.auth.userData
+
+export default authSlice.reducer
