f617ac5b184bc6147d93e3e167bb52c3c48ee5fe
gabyMangoni <mangonigabriela@gmail.com>
2021-12-08 17:43:16 -0300

    edit index redux

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-<<<<<<< HEAD:src/index.js
 import { Provider } from 'react-redux'
-=======
->>>>>>> 7a459fefb16525eac670b266b5aa129d14cc9e39:index.js
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
+import store from './app/store'
 import * as serviceWorker from './serviceWorker'
 import theme from './themes'
-<<<<<<< HEAD:src/index.js
-=======
-// eslint-disable-next-line
-import '@fontsource/atma/400.css'
->>>>>>> 7a459fefb16525eac670b266b5aa129d14cc9e39:index.js
 
 ReactDOM.render(
   <ChakraProvider theme={theme}>
     <React.StrictMode>
-<<<<<<< HEAD:src/index.js
       <Provider store={store}>
         <App />
       </Provider>
-=======
-      <App />
->>>>>>> 7a459fefb16525eac670b266b5aa129d14cc9e39:index.js
     </React.StrictMode>
   </ChakraProvider>,
   document.getElementById('root'),
