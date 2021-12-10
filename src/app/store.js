d335fc9b924141d9b1de031cfa38ca74dc51a1e3
alexiscjscab <alexiscjscab@gmail.com>
2021-12-09 21:54:10 -0300

    Modify ContactsSlice and store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import auth from './slices/auth'
-import counterReducer from '../features/counter/counterSlice'
-import contactsreducer from '../features/contacts/contactsSlice'
+import contactsreducer from './slices/contacts'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     contacts: contactsreducer,
     auth,
   },
