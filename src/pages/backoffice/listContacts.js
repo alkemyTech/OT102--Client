2368bfadab47fdcdefdc3c31287d992868920ffa
alexiscjscab <alexiscjscab@gmail.com>
2021-12-10 20:53:40 -0300

    Fix package-lock.json

--- src/pages/backoffice/listContacts.js
+++ src/pages/backoffice/listContacts.js
@@ -21,7 +21,7 @@ const ListContacts = () => {
         const response = await getAllContacts()
         setContacts(response.data.body)
       } catch (error) {
-        // agregar  alert mas adelante
+        // Agregar el alert mas adelante para mostrar el error
         // eslint-disable-next-line no-console
         console.log(error)
       }
