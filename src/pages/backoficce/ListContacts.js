9fca2f381593bcabb358de5cb41043b8baad6fae
Alexis Beas <74843944+alexiscjscab@users.noreply.github.com>
2021-12-12 18:37:37 -0300

    Pages List Contacts (#22)

---
+++ src/pages/backoficce/ListContacts.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react'
+import {
+  Box,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Text,
+} from '@chakra-ui/react'
+import Spinner from '../../components/Spinner'
+import { getAllContacts } from '../../services/contactsService'
+
+const ListContacts = () => {
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const response = await getAllContacts()
+        setContacts(response.data.body)
+      } catch (error) {
+        // Agregar el alert mas adelante para mostrar el error
+        // eslint-disable-next-line no-console
+        console.log(error)
+      }
+    }
+    getContacts()
+  }, [])
+
+  return (
+    <Box
+      mt="30px"
+      d="flex"
+      justifyContent="center"
+      alignItems="center"
+      p="5px"
+      flexDirection="column"
+      textAlign="center"
+    >
+      <Text fontSize="2xl" mb="30px">Contactos</Text>
+      {contacts.length > 0 ? (
+        <Table size="sm" textAlign="center">
+          <Thead bg="brand.cyan">
+            <Tr>
+              <Th textAlign="center">
+                nombre
+              </Th>
+              <Th textAlign="center">
+                telefono
+              </Th>
+              <Th textAlign="center">
+                email
+              </Th>
+              <Th textAlign="center">
+                mensaje
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {contacts.map((item) => (
+              <Tr key={item.id}>
+                <Td textAlign="center">
+                  {item.name}
+                </Td>
+                <Td textAlign="center">
+                  {item.phone}
+                </Td>
+                <Td textAlign="center">
+                  {item.email}
+                </Td>
+                <Td textAlign="center">
+                  {item.message}
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      ) : (
+        <Spinner />
+      )}
+    </Box>
+  )
+}
+
+export default ListContacts
