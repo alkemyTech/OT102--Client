4f5e864a4e69b35ce8909d4d616d18f29dd4370a
maxicuyo94 <maxibc94@gmail.com>
2021-12-12 20:43:31 -0300

    MyProfile

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Formik, Form } from 'formik'
+
+import * as Yup from 'yup'
+
 import {
   Button,
   Flex,
@@ -17,99 +21,222 @@ import {
 } from '@chakra-ui/react'
 import { SmallCloseIcon } from '@chakra-ui/icons'
 import Spinner from './Spinner'
+import { updateuser, delUser } from '../services/usersService'
+import Alert from './alert/Alert'
+import FormikControl from '../pages/contact/FormikControl'
+import useUser from '../hooks/useUser'
 
 export default function MyProfile() {
   const [loading, setLoading] = React.useState(false)
   const [edit, setEdit] = React.useState(false)
-  const [data, setData] = React.useState({
-    firstName: 'Maxi',
-    lastName: 'Menendez',
-    email: 'maxim@menedez.com',
+  const { userData: data } = useUser()
+  const [alertProps, setAlertprops] = React.useState({
+    show: false,
+    title: '',
+    message: '',
+    icon: '',
+    onConfirm: () => { },
   })
+
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required('⚠ Este dato es obligatorio'),
+    lastName: Yup.string().required('⚠ Este dato es obligatorio'),
+    email: Yup.string()
+      .email('❌ El email no es válido')
+      .required('⚠ Este dato es obligatorio'),
+    image: Yup.string().required('⚠ Este dato es obligatorio'),
+  })
+  const initialValues = {
+    id: data?.id,
+    firstName: data?.firstName,
+    lastName: data?.lastName,
+    email: data?.email,
+    image: data?.image,
+  }
+  const closeAlert = () => {
+    setAlertprops({
+      show: false,
+      title: '',
+      message: '',
+      icon: '',
+      onConfirm: () => { },
+    })
+  }
+  const handleSubmit = async (values) => {
+    setLoading(true)
+
+    if (edit) {
+      const { id, ...user } = values
+      try {
+        await updateuser(id, user)
+        setAlertprops({
+          show: true,
+          title: '¡Éxito!',
+          message: 'El usuario se ha actualizado correctamente',
+          icon: 'check-circle',
+          onConfirm: closeAlert,
+        })
+
+      } catch (error) {
+        setAlertprops({
+          show: true,
+          title: '¡Error!',
+          message: 'Ha ocurrido un error al actualizar el usuario',
+          icon: 'warning',
+          onConfirm: closeAlert,
+        })
+
+      }
+    }
+    else {
+      setEdit(true)
+    }
+    setLoading(false)
+  }
+  const handleCancel = async () => {
+    if (edit) {
+      setEdit(false)
+    } else {
+      //:todo handle delet
+      setAlertprops({
+        show: true,
+        title: '¿Seguro que quieres eliminar tu cuenta?',
+        message: 'Esta acción no se puede deshacer',
+        icon: 'warning',
+        cancelbtn: true,
+        onConfirm: async () => {
+          try {
+            await delUser(data.id)
+            setAlertprops({
+              show: true,
+              title: '¡Éxito!',
+              message: 'El usuario se ha eliminado correctamente',
+              icon: 'check-circle',
+              onConfirm: closeAlert,
+            })
+
+          } catch (error) {
+            setAlertprops({
+              show: true,
+              title: '¡Error!',
+              message: 'Ha ocurrido un error al eliminar el usuario',
+              icon: 'warning',
+              onConfirm: closeAlert,
+            })
+
+          }
+          console.log('delete')
+        },
+        onCancel: closeAlert,
+      }
+      )
+    }
+  }
 
   if (loading) {
     return <Spinner />
   }
-
   return (
-    <Flex minH="100%" align="center" justify="center" bg="grey.100">
-      <Stack spacing={4} w="full" maxW="md" bg="white" rounded="xl" boxShadow="lg" p={6} my={12}>
-        <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
-          {edit ? 'Editar Perfil' : 'Mi Perfil'}
-        </Heading>
-        <FormControl id="userName">
-          <FormLabel>User Icon</FormLabel>
-          <Stack direction={edit ? ['column', 'row'] : 'column'} spacing={6}>
-            <Center>
-              <Avatar size="xl" src="https://bit.ly/sage-adebayo">
-                {edit ? (
-                  <AvatarBadge
-                    as={IconButton}
-                    size="sm"
-                    rounded="full"
-                    top="-10px"
-                    colorScheme="red"
-                    aria-label="remove Image"
-                    icon={<SmallCloseIcon />}
-                  />
-                ) : null}
-              </Avatar>
-            </Center>
-            {edit ? (
-              <Center w="full">
-                <Button w="full">Cambiar imagen</Button>
-              </Center>
-            ) : null}
-          </Stack>
-        </FormControl>
-        <HStack>
-          <Box>
-            <FormControl id="firstName" isRequired>
-              <FormLabel>First Name</FormLabel>
-              <Input
-                type="text"
-                disabled={!edit}
-                defaultValue={data.firstName}
-                _disabled={{ opacity: 1 }}
-              />
-            </FormControl>
-          </Box>
-          <Box>
-            <FormControl id="lastName">
-              <FormLabel>Last Name</FormLabel>
-              <Input
-                type="text"
-                disabled={!edit}
-                defaultValue={data.lastName}
-                _disabled={{ opacity: 1 }}
-              />
-            </FormControl>
-          </Box>
-        </HStack>
-        <FormControl id="email" isRequired>
-          <FormLabel>Email address</FormLabel>
-          <Input
-            placeholder="your-email@example.com"
-            _placeholder={{ color: 'gray.500' }}
-            type="email"
-            disabled={!edit}
-            defaultValue={data.email}
-            _disabled={{ opacity: 1 }}
-          />
-        </FormControl>
-
-        <Stack spacing={6} direction={['column', 'row']}>
-          <Button
-            bg="blue.400"
-            color="white"
-            w="full"
-            _hover={{
-              bg: 'blue.500',
-            }}
-            onClick={() => (!edit ? setEdit(!edit) : null)}
+    <>
+      <Alert {...alertProps} />
+      <Flex minH="100%" align="center" justify="center" bg="grey.100">
+        <Stack
+          spacing={4}
+          w="full"
+          maxW="md"
+          bg="white"
+          rounded="xl"
+          boxShadow="lg"
+          p={6}
+          my={4}
+        >
+          <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
+            {edit ? 'Editar Perfil' : 'Mi Perfil'}
+          </Heading>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
           >
-            {edit ? 'Guardar' : 'Editar'}
-          </Button>
+            <Form onSubmit={handleSubmit}>
+              <FormControl id="userName">
+                <FormLabel>User Icon</FormLabel>
+                <Stack
+                  direction={edit ? ['column', 'row'] : 'column'}
+
+                >
+                  <Center>
+                    <Avatar size="xl" src={data?.image}>
+                      {/* todo:  */}
+                      {edit ? (
+                        <AvatarBadge
+                          as={IconButton}
+                          size="sm"
+                          rounded="full"
+                          top="-10px"
+                          colorScheme="red"
+                          aria-label="remove Image"
+                          icon={<SmallCloseIcon />}
+                        />
+                      ) : null}
+                    </Avatar>
+                  </Center>
+                  {edit ? (
+                    <Center w="full">
+                      <Button w="full">Cambiar imagen</Button>
+                    </Center>
+                  ) : null}
+                </Stack>
+              </FormControl>
+              <HStack>
+                <Box my={4}>
+                  <FormikControl
+                    control="chakraInput"
+                    type="text"
+                    name="firstName"
+                    label="Nombre"
+                    _disabled={{ opacity: 1 }}
+                    disabled={!edit}
+
+                  />
+
+
+                </Box>
+                <Box>
+                  <FormikControl
+                    control="chakraInput"
+                    type="text"
+                    name="lastName"
+                    label="Apellido"
+                    _disabled={{ opacity: 1 }}
+                    disabled={!edit}
+                  />
+                </Box>
+              </HStack>
+              <FormikControl
+                control="chakraInput"
+                type="email"
+                name="email"
+                label="Email"
+                _disabled={{ opacity: 1 }}
+                disabled={!edit}
+              />
+
+              <Button
+                type='submit'
+                bg="blue.400"
+                color="white"
+                w="full"
+                _hover={{
+                  bg: 'blue.500',
+                }}
+                mt={4}
+              >
+                {edit ? 'Guardar' : 'Editar'}
+              </Button>
+
+            </Form>
+          </Formik>
           <Button
             bg="red.400"
             color="white"
@@ -117,12 +244,12 @@ export default function MyProfile() {
             _hover={{
               bg: 'red.500',
             }}
-            onClick={() => (edit ? setEdit(!edit) : null)}
+            onClick={handleCancel}
           >
             {edit ? 'Cancelar' : 'Eliminar'}
           </Button>
         </Stack>
-      </Stack>
-    </Flex>
+      </Flex>
+    </>
   )
 }
