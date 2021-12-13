cba469e204ec4a85aab909a0e0cd23b2bd0036ed
gabyMangoni <mangonigabriela@gmail.com>
2021-12-13 00:24:12 -0300

    add screen detail news

--- src/pages/NewsDetail.js
+++ src/pages/NewsDetail.js
@@ -1,15 +1,33 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+// import { useParams } from 'react-router-dom'
+import {
+  Container,
+  Text,
+  VStack,
+  Image,
+} from '@chakra-ui/react';
 
-export default function NewsDetail() {
-  const params = useParams()
-  return (
+const NewsDetail = () =>
+// const { id } = useParams()
+  (
     <>
-      <h1>NewsDetail page</h1>
-      <h2>
-        news:
-        {params.id}
-      </h2>
+      <Container maxW="container.lg" mt="5">
+        <VStack paddingTop="40px" spacing="2" alignItems="flex-start">
+          <Image
+            w="full"
+            h="250px"
+            src="https://www.unicef.org/argentina/sites/unicef.org.argentina/files/styles/press_release_feature/public/DeclaracioHenrietta.jpg?itok=j4DT4DR_"
+            objectFit="cover"
+            objectPosition="center -100px"
+          />
+
+          <Text textStyle="title">Titulo de la Novedad</Text>
+          <Text as="p" fontSize="lg">
+            Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos deesde el año
+          </Text>
+        </VStack>
+      </Container>
     </>
   )
-}
+
+export default NewsDetail;
