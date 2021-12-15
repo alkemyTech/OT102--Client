73eb70d98879821ddc25d8b8d879e41a14bf27ae
gabyMangoni <79479608+gabyMangoni@users.noreply.github.com>
2021-12-14 21:02:16 -0300

    [SCREEN] Detail news - OT102-49 (#25)
    * add screen detail news
    * obtaining data from detail endpoint
    * add spinner and edit txt

--- src/pages/NewsDetail.js
+++ src/pages/NewsDetail.js
@@ -1,15 +1,82 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import {
+  Container,
+  Text,
+  VStack,
+  Image,
+} from '@chakra-ui/react'
+import { getEntryById } from '../services/entriesService'
+import Alert from '../components/alert/Alert'
+import Spinner from '../components/Slider'
 
 export default function NewsDetail() {
-  const params = useParams()
+  const { id } = useParams()
+  const [alertProps, setAlertprops] = useState({
+    show: false,
+    title: '',
+    message: '',
+    icon: '',
+    onConfirm: () => {},
+  })
+
+  const [newsData, setNewsData] = useState({
+    image: '',
+    name: '',
+    content: '',
+  })
+
+  const [loading, setLoading] = useState(false)
+
+  const loadData = async () => {
+    try {
+      setLoading(true)
+      const loadedNews = await getEntryById(id)
+      setNewsData(loadedNews.data.body)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      const errorAlertProps = {
+        show: true,
+        title: 'Ooops, algo ha fallado!',
+        message: error.message,
+        icon: 'error',
+        onConfirm: () => {},
+      }
+      setAlertprops(errorAlertProps)
+    }
+  }
+
+  useEffect(() => {
+    if (id) {
+      loadData()
+    }
+  // eslint-disable-next-line
+  }, [])
+
+  if (loading) {
+    return (
+      <Spinner />
+    )
+  }
+
   return (
     <>
-      <h1>NewsDetail page</h1>
-      <h2>
-        news:
-        {params.id}
-      </h2>
+      <Alert {...alertProps} />
+      <Container maxW="container.lg" mt="5">
+        <VStack paddingTop="40px" spacing="2" alignItems="flex-start">
+          <Image
+            w="full"
+            h="250px"
+            src={newsData.image}
+            objectFit="cover"
+            objectPosition="center -100px"
+          />
+
+          <Text textStyle="title">{newsData.name}</Text>
+          <Text as="p" fontSize="lg">{newsData.content}</Text>
+        </VStack>
+      </Container>
     </>
   )
 }
