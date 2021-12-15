6e0c17cd1506cb79a0bacc236af00ded478d2c78
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-12-14 21:06:49 -0300

    add newUser function hook

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,15 +1,17 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { selectUserData, setUserData } from '../app/slices/auth'
 import { registerUser } from '../services/authService'
 
 export default function useUser() {
   const userData = useSelector(selectUserData)
   const jwt = window.localStorage.getItem('x-access-token')
+  const dispatch = useDispatch()
 
-  const newUser = (data) =>
-    registerUser(data).then(({ body }) => {
-      setUserData(body)
-      window.localStorage.set('x-access-token', body.token)
+  const newUser = (user) =>
+    registerUser(user).then(({ data }) => {
+      const { body } = data
+      dispatch(setUserData(user))
+      window.localStorage.setItem('x-access-token', body.token)
     })
 
   return {
