d0928580d68de0a32b8423ae9054e47486c1f1a5
Gabriel Martinez Corti <79473217+gabodmc@users.noreply.github.com>
2021-12-16 10:37:58 -0300

    [ FORMS] ACTIVITIES FORM - OT102-61 (#34)
    * Reusable activity Form with CKEditor added
    * Unifying Yup schemas in one validationSchema file

---
+++ src/components/forms/ValidationSchemas.js
@@ -0,0 +1,20 @@
+import * as Yup from 'yup'
+
+const ActivitySchema = Yup.object().shape({
+  name: Yup.string()
+    .required('Nombre es obligatorio')
+    .min(3, 'El nombre debe tener mas de dos caracteres')
+    .max(255, 'Demasiado largo!'),
+})
+
+const CategorySchema = Yup.object().shape({
+  categoryName: Yup.string()
+    .required('Nombre es obligatorio')
+    .min(3, 'El nombre debe tener mas de dos caracteres')
+    .max(255, 'Demasiado largo!'),
+  description: Yup.string()
+    .required('La descripción es obligatoria')
+    .min(3, 'La descripción debe tener mas de dos caracteres'),
+})
+
+export { ActivitySchema, CategorySchema }
