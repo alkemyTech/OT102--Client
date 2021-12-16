f92a70c5c10bc30a82fa47ebe1ec69f3c84d4cfc
joselo85 <joseignaciolorenzo85@gmail.com>
2021-12-16 12:10:37 -0300

    Fixed minor bugs

--- src/services/imgUploadService.js
+++ src/services/imgUploadService.js
@@ -5,7 +5,7 @@ const config = {
   dirName: process.env.REACT_APP_AWS_S3_DIRNAME,
   region: process.env.REACT_APP_AWS_S3_REGION,
   accessKeyId: process.env.REACT_APP_AWS_S3_KEY,
-  secretAccessKey: process.env.REACT_APP_AWS_S3_KEY,
+  secretAccessKey: process.env.REACT_APP_AWS_S3_SECRET,
   s3Url: process.env.REACT_APP_AWS_S3_URL,
 }
 
@@ -17,14 +17,20 @@ const ReactS3Client = new S3(config)
  * @return Returns the URL where the file is located
  */
 
+// eslint-disable-next-line consistent-return
 const imgUploadService = async (file) => {
   const uuid = crypto.randomUUID() // Generate a Universally Unique Identifier.
   const fileNameArr = file.name.split('.') // Split the file name into arrays when it finds '.'
   const fileExtension = fileNameArr[fileNameArr.length - 1] // Extract just the file extension.
   const uniqueFileName = `${uuid}.${fileExtension}` // Create the new Unique filename.
-  await ReactS3Client.uploadFile(file, uniqueFileName)
-    .then((response) => response.location) // Returns the URL where the file is located.
-    .catch((error) => new Error(error))
+  try {
+    const upload = await ReactS3Client.uploadFile(file, uniqueFileName)
+    if (upload) {
+      return upload.location
+    }
+  } catch (error) {
+    throw new Error(error.message)
+  }
 }
 
 export default imgUploadService
