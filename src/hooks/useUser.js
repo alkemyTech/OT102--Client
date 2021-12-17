1c6be22cf2886d92e6ca83be3c05adcfb2854152
gabyMangoni <79479608+gabyMangoni@users.noreply.github.com>
2021-12-16 22:27:45 -0300

    Connect Authentication Frontend Backend (#42)

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { selectUserData, setUserData, destroyUserData } from '../app/slices/auth'
-import { registerUser } from '../services/authService'
+import { userLogin, registerUser } from '../services/authService'
 
 export default function useUser() {
   const userData = useSelector(selectUserData)
@@ -14,12 +14,19 @@ export default function useUser() {
       window.localStorage.setItem('x-access-token', body.token)
     })
 
+  const loginUser = (user) =>
+    userLogin(user).then(({ data }) => {
+      const { body } = data
+      dispatch(setUserData(body.user))
+      window.localStorage.setItem('x-access-token', body.token)
+    })
   const logoutUser = () => {
     window.localStorage.removeItem('x-access-token')
     dispatch(destroyUserData())
   }
 
   return {
+    loginUser,
     newUser,
     logoutUser,
     isLogged: Boolean(jwt),
