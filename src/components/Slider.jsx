97cf70e182384472ffe7bde5afd6d8e8e7dc6962
Jonatan Garbuyo <58077441+JonatanGarbuyo@users.noreply.github.com>
2021-12-17 17:55:30 -0300

    [SCREEN] fix home screen - FIX (#45)
    * fix home screen
    
    * fix slint error
    
    * fix import error

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,35 +3,31 @@ import PropTypes from 'prop-types'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import { Carousel } from 'react-responsive-carousel'
 
-const Slider = ({ slide, height, ...props }) =>
-  (
-    <Carousel
-      {...props}
-      infiniteLoop
-      useKeyboardArrows
-      autoPlay
-      showThumbs={false}
-      showStatus={false}
-      showArrows={false}
-    >
-      {
-        slide.map(({ imgUrl, text }) => (
-          <div>
-            <img src={imgUrl} alt={text} height={height} />
-          </div>
-        ))
-      }
-    </Carousel>
-  )
+const Slider = ({ slides, ...rest }) => (
+  <Carousel
+    infiniteLoop
+    useKeyboardArrows
+    autoPlay
+    showThumbs={false}
+    showStatus={false}
+    showArrows={false}
+    {...rest}
+  >
+    {slides.map(({ imgUrl, text }) => (
+      <div key={imgUrl}>
+        <img src={imgUrl} alt={text} />
+      </div>
+    ))}
+  </Carousel>
+)
 
 Slider.propTypes = {
-  slide: PropTypes.shape([]),
-  height: PropTypes.number,
-}
-
-Slider.defaultProps = {
-  slide: [],
-  height: 300,
+  slides: PropTypes.arrayOf(
+    PropTypes.shape({
+      imgUrl: PropTypes.string,
+      text: PropTypes.string,
+    }),
+  ).isRequired,
 }
 
 export default Slider
