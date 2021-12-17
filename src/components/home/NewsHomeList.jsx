4765a258d64d046adca843076f919bdb51c82b2c
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-12-17 00:03:58 -0300

    fix home screen

--- src/components/home/NewsHomeList.jsx
+++ src/components/home/NewsHomeList.jsx
@@ -1,53 +1,43 @@
 import React, { useState, useEffect } from 'react'
-import { Box, SimpleGrid } from '@chakra-ui/react'
+import { HStack, SimpleGrid } from '@chakra-ui/react'
 import { getAllEntries } from '../../services/entriesService'
 import NewsHomeCard from '../news/NewsHomeCard'
 import Spinner from '../Spinner'
 
 const ListNews = () => {
+  const [isLoading, setIsLoading] = useState(false)
   const [allNews, setNews] = useState([])
-  const [error, setError] = useState(null)
 
-  const loadData = async () => {
-    getAllEntries().then((entries) => {
-      setNews(entries.data.body)
-      setError(null)
-    }).catch((err) =>
-      setError(err))
-  }
-
-  // run on load
   useEffect(() => {
-    loadData()
+    setIsLoading(true)
+    getAllEntries()
+      .then(({ data }) => {
+        setNews(data.body)
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        setIsLoading(false)
+        // eslint-disable-next-line no-console
+        console.log(error)
+      })
   }, [])
-
-  const getListItems = () =>
-    (
-      <Box p={4}>
-        <SimpleGrid columns={{ base: 1, md: 4 }} spacing={6}>
-
-          { !allNews ? <Spinner /> : allNews.map((news) =>
-            <NewsHomeCard {...news} key={news.id} />).reverse().slice(0, 4)}
-        </SimpleGrid>
-      </Box>
-    )
-
-  const getErrorView = () =>
-    (
-      <div>
-        Error al cargar las novedades!
-        { error.message }
-        <Spinner />
-      </div>
-    )
 
   return (
     <div>
-      <ul>
-        { error ? getErrorView() : getListItems() }
-      </ul>
+      <HStack p={4} wrap="wrap">
+        <SimpleGrid columns={{ base: 1, md: 4 }} spacing={6}>
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            allNews
+              .map((news) => <NewsHomeCard {...news} key={news.id} />)
+              .reverse()
+              .slice(0, 4)
+          )}
+        </SimpleGrid>
+      </HStack>
     </div>
   )
 }
 
-export default ListNews;
+export default ListNews
