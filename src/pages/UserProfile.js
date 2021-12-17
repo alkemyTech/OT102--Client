dd3811208033e55dddae575ed2a1728ca1e4a5c2
maxicuyo94 <maxibc94@gmail.com>
2021-12-17 12:59:57 -0300

    user profile

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
-import { Formik, Form } from 'formik'
-import * as Yup from 'yup'
-
 import {
   Button,
   Flex,
@@ -12,22 +8,16 @@ import {
   Stack,
   HStack,
   Avatar,
-  AvatarBadge,
-  IconButton,
   Center,
   Box,
+  Input,
 } from '@chakra-ui/react'
-import { SmallCloseIcon } from '@chakra-ui/icons'
-import FormikControl from './contact/FormikControl'
-import Spinner from '../components/Spinner'
-import { updateuser, delUser } from '../services/usersService'
+import { useNavigate } from 'react-router-dom'
+import { delUser } from '../services/usersService'
 import Alert from '../components/alert/Alert'
 import useUser from '../hooks/useUser'
 
 export default function UserProfile() {
-  const params = useParams()
-  const [loading, setLoading] = React.useState(false)
-  const [edit, setEdit] = React.useState(false)
   const { userData: data } = useUser()
   const [alertProps, setAlertprops] = React.useState({
     show: false,
@@ -36,196 +26,118 @@ export default function UserProfile() {
     icon: '',
     onConfirm: () => {},
   })
-
-  const validationSchema = Yup.object({
-    firstName: Yup.string().required('⚠ Este dato es obligatorio').max(255, '⚠ Demasiado largo!'),
-    lastName: Yup.string().required('⚠ Este dato es obligatorio').max(255, '⚠ Demasiado largo!'),
-    email: Yup.string().email('❌ El email no es válido').required('⚠ Este dato es obligatorio'),
-    image: Yup.string().required('⚠ Este dato es obligatorio'),
-  })
-  const initialValues = {
-    id: data ? data.id : params.id,
-    firstName: data ? data.firstName : '',
-    lastName: data ? data.lastName : '',
-    email: data ? data.email : '',
-    image: data ? data.image : '',
+  const navigate = useNavigate()
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    navigate('/backoffice/me')
   }
-  const closeAlert = () => {
+  const handleDelete = async (e) => {
+    e.preventDefault()
     setAlertprops({
-      show: false,
-      title: '',
-      message: '',
-      icon: '',
-      onConfirm: () => {},
+      show: true,
+      title: '¿Estás seguro?',
+      message: '¿Estás seguro que quieres eliminar tu cuenta?, esta acción no se puede deshacer',
+      icon: 'warning',
+      onConfirm: () => {
+        delUser(data.id)
+          .then(() => {
+            navigate('/')
+          })
+          .catch(() => {
+            setAlertprops({
+              show: true,
+              title: 'Error',
+              message: 'No se pudo eliminar tu cuenta',
+              icon: 'error',
+              onConfirm: () => {
+                setAlertprops({
+                  show: false,
+                  title: '',
+                  message: '',
+                  icon: '',
+                  onConfirm: () => {},
+                })
+              },
+            })
+          })
+      },
     })
   }
-  const handleSubmit = async (values) => {
-    values.preventDefault()
 
-    setLoading(true)
-
-    if (edit) {
-      const { id, ...user } = values
-      try {
-        await updateuser(id, user)
-        setAlertprops({
-          show: true,
-          title: '¡Éxito!',
-          message: 'El usuario se ha actualizado correctamente',
-          icon: 'check-circle',
-          onConfirm: closeAlert,
-        })
-      } catch (error) {
-        setAlertprops({
-          show: true,
-          title: '¡Error!',
-          message: 'Ha ocurrido un error al actualizar el usuario',
-          icon: 'warning',
-          onConfirm: closeAlert,
-        })
-      }
-    } else {
-      setEdit(true)
-    }
-    setLoading(false)
-  }
-  const handleCancel = async (e) => {
-    e.preventDefault()
-    if (edit) {
-      setEdit(false)
-    } else {
-      // :todo handle delet
-      setAlertprops({
-        show: true,
-        title: '¿Seguro que quieres eliminar tu cuenta?',
-        message: 'Esta acción no se puede deshacer',
-        icon: 'warning',
-        cancelbtn: true,
-        onConfirm: async () => {
-          try {
-            await delUser(data.id)
-            setAlertprops({
-              show: true,
-              title: '¡Éxito!',
-              message: 'El usuario se ha eliminado correctamente',
-              icon: 'check-circle',
-              onConfirm: closeAlert,
-            })
-          } catch (error) {
-            setAlertprops({
-              show: true,
-              title: '¡Error!',
-              message: 'Ha ocurrido un error al eliminar el usuario',
-              icon: 'warning',
-              onConfirm: closeAlert,
-            })
-          }
-          // console.log('delete')
-        },
-        onCancel: closeAlert,
-      })
-    }
-  }
-
-  if (loading) {
-    return <Spinner />
-  }
   return (
     <>
       <Alert {...alertProps} />
       <Flex minH="100%" align="center" justify="center" bg="grey.100">
         <Stack spacing={4} w="full" maxW="md" bg="white" rounded="xl" boxShadow="lg" p={6} my={4}>
           <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
-            {edit ? 'Editar Perfil' : 'Mi Perfil'}
+            Mi Perfil
           </Heading>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-          >
-            <Form onSubmit={handleSubmit}>
-              <FormControl id="userName">
-                <FormLabel>User Icon</FormLabel>
-                <Stack direction={edit ? ['column', 'row'] : 'column'}>
-                  <Center>
-                    <Avatar size="xl" src={data ? data.image : ''}>
-                      {/* todo:  */}
-                      {edit ? (
-                        <AvatarBadge
-                          as={IconButton}
-                          size="sm"
-                          rounded="full"
-                          top="-10px"
-                          colorScheme="red"
-                          aria-label="remove Image"
-                          icon={<SmallCloseIcon />}
-                        />
-                      ) : null}
-                    </Avatar>
-                  </Center>
-                  {edit ? (
-                    <Center w="full">
-                      <Button w="full">Cambiar imagen</Button>
-                    </Center>
-                  ) : null}
-                </Stack>
-              </FormControl>
-              <HStack>
-                <Box my={4}>
-                  <FormikControl
-                    control="chakraInput"
-                    type="text"
-                    name="firstName"
-                    label="Nombre"
-                    _disabled={{ opacity: 1 }}
-                    disabled={!edit}
-                  />
-                </Box>
-                <Box>
-                  <FormikControl
-                    control="chakraInput"
-                    type="text"
-                    name="lastName"
-                    label="Apellido"
-                    _disabled={{ opacity: 1 }}
-                    disabled={!edit}
-                  />
-                </Box>
-              </HStack>
-              <FormikControl
-                control="chakraInput"
-                type="email"
-                name="email"
-                label="Email"
-                _disabled={{ opacity: 1 }}
-                disabled={!edit}
-              />
 
-              <Button
-                type="submit"
-                bg="blue.400"
-                color="white"
-                w="full"
-                _hover={{
-                  bg: 'blue.500',
-                }}
-                mt={4}
-              >
-                {edit ? 'Guardar' : 'Editar'}
-              </Button>
-            </Form>
-          </Formik>
-          <Button
-            bg="red.400"
-            color="white"
-            w="full"
-            _hover={{
-              bg: 'red.500',
-            }}
-            onClick={handleCancel}
-          >
-            {edit ? 'Cancelar' : 'Eliminar'}
-          </Button>
+          <FormControl id="userName">
+            <Stack direction="column">
+              <Center>
+                <Avatar size="xl" src={data ? data.image : ''} />
+              </Center>
+            </Stack>
+          </FormControl>
+          <HStack>
+            <Box>
+              <FormLabel>Nombre</FormLabel>
+              <Input
+                name="name"
+                placeholder="Nombre"
+                value={data ? data.firstName : ''}
+                disabled
+                _disabled={{ opacity: 1 }}
+              />
+            </Box>
+            <Box>
+              <FormLabel>Apellido</FormLabel>
+              <Input
+                name="lastName"
+                placeholder="Apellido"
+                value={data ? data.lastName : ''}
+                disabled
+                _disabled={{ opacity: 1 }}
+              />
+            </Box>
+          </HStack>
+          <Box>
+            <FormLabel>Email</FormLabel>
+            <Input
+              name="email"
+              placeholder="Email"
+              value={data ? data.email : ''}
+              disabled
+              _disabled={{ opacity: 1 }}
+            />
+          </Box>
+
+          <HStack>
+            <Button
+              onClick={handleSubmit}
+              bg="blue.400"
+              color="white"
+              w="full"
+              _hover={{
+                bg: 'blue.500',
+              }}
+            >
+              Editar
+            </Button>
+
+            <Button
+              onClick={handleDelete}
+              bg="red.400"
+              color="white"
+              w="full"
+              _hover={{
+                bg: 'red.500',
+              }}
+            >
+              Eliminar
+            </Button>
+          </HStack>
         </Stack>
       </Flex>
     </>
