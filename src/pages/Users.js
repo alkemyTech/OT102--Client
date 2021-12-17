2655e132f57a4be670cdd0343fde474c7fab723f
Alexis Beas <74843944+alexiscjscab@users.noreply.github.com>
2021-12-17 18:23:06 -0300

    [ SCREEN ] [ BACKOFFICE ] USERS LIST  - OT102-51 (#40)
    * List Users
    
    * Link to edit user
    
    * pages users

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import ListUsers from './backoffice/users/ListUsers'
+// import { Outlet } from 'react-router-dom'
 
 export default function Users() {
   return (
     <>
       <h1>List of users</h1>
-      <Outlet />
+      <ListUsers />
+      {/* <Outlet /> */}
     </>
   )
 }
