735a08328ef65200333b56b7b23dec5d9b3ff4a0
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-12-20 22:22:22 -0300

    fix categories

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -2,10 +2,5 @@ import React from 'react'
 import ListCategories from './backoffice/categories/ListCategories'
 
 export default function Categories() {
-  return (
-    <div>
-      list of categories page
-      <ListCategories />
-    </div>
-  )
+  return <ListCategories />
 }
