6932251ca3be74cd3c076c82dbb240ee5539f0fd
gabyMangoni <79479608+gabyMangoni@users.noreply.github.com>
2021-12-21 19:01:01 -0300

    add list testimonial and card (#54)

---
+++ src/components/testimonials/CardTestimonials.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  Avatar,
+  Center,
+  chakra,
+  Flex,
+  useColorModeValue,
+} from '@chakra-ui/react'
+
+const TestimonialCard = ({
+  name,
+  content,
+  image,
+  id,
+  ...props
+}) => (
+  <Center {...props} key={id}>
+    <Flex
+      boxShadow="lg"
+      maxW="640px"
+      direction={{ base: 'column-reverse', md: 'row' }}
+      width="full"
+      rounded="xl"
+      p={10}
+      justifyContent="space-between"
+      position="relative"
+      bg={useColorModeValue('white', 'gray.800')}
+      _after={{
+        content: '""',
+        position: 'absolute',
+        height: '21px',
+        width: '29px',
+        left: '35px',
+        top: '-10px',
+        backgroundSize: 'cover',
+        backgroundImage: 'https://e7.pngegg.com/pngimages/264/841/png-clipart-quotation-mark-greinarmerki-punctuation-quiromasaje-signo-comillas-blue-text.png',
+      }}
+      _before={{
+        content: '""',
+        position: 'absolute',
+        zIndex: '-1',
+        height: 'full',
+        maxW: '640px',
+        width: 'full',
+        filter: 'blur(40px)',
+        transform: 'scale(0.98)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        top: 0,
+        left: 0,
+      }}
+    >
+      <Flex direction="column" textAlign="left" justifyContent="space-between">
+        <chakra.p fontWeight="400" fontSize="15px" pb={4}>
+          {content}
+        </chakra.p>
+        <chakra.p fontWeight="bold" fontSize={14}>
+          {name}
+        </chakra.p>
+      </Flex>
+      <Avatar
+        src={image}
+        height="80px"
+        width="80px"
+        alignSelf="center"
+        m={{ base: '0 0 35px 0', md: '0 0 0 50px' }}
+      />
+    </Flex>
+  </Center>
+)
+
+TestimonialCard.propTypes = {
+  id: PropTypes.number,
+  image: PropTypes.string,
+  name: PropTypes.string,
+  content: PropTypes.string,
+}
+
+TestimonialCard.defaultProps = {
+  id: '1',
+  image:
+    'https://www.unicef.org/argentina/sites/unicef.org.argentina/files/styles/hero_desktop/public/Unicef-Widnicky006.jpg?itok=2Du-9s5F',
+  name: 'testimonio',
+  content: 'Testimonios Somos Mas',
+}
+
+export default TestimonialCard
